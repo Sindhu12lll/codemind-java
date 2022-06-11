9a02dcad600e0ef01beac4d95cd615fd1c0fcf5b
Sindhu12lll <96376069+Sindhu12lll@users.noreply.github.com>
2022-06-11 21:35:09 +0530

    import java.util.Scanner; class coin_game {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         if(a%2==0||b%2==0)         {             System.out.println("Player 1");         }         else         {             System.out.println("Player 2");         }     } }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class coin_game
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a%2==0||b%2==0)
+        {
+            System.out.println("Player 1");
+        }
+        else
+        {
+            System.out.println("Player 2");
+        }
+    }
+}
